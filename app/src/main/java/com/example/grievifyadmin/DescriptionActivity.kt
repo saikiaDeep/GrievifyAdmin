bb74f45842d9321353eed1db9357f3c59466799b
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-04-06 19:10:16 +0530

    Description screen UI update

--- app/src/main/java/com/example/grievifyadmin/DescriptionActivity.kt
+++ app/src/main/java/com/example/grievifyadmin/DescriptionActivity.kt
@@ -24,64 +24,64 @@ class DescriptionActivity : AppCompatActivity() {
         val extras = intent.extras
         if (extras != null) {
             itemID = extras.getString("key").toString()
-            fetchDataFromDataBase(itemID)
+            //fetchDataFromDataBase(itemID)
         }
     }
-    private fun fetchDataFromDataBase(items: String) {
-        val databaseItem =
-            FirebaseDatabase.getInstance()
-                .getReference("tickets")
-
-        databaseItem.child(items).get().addOnSuccessListener { dataSnapshot ->
-            //Toast.makeText(applicationContext,dataSnapshot.child("userID").value.toString(),Toast.LENGTH_SHORT).show()
-            println(dataSnapshot)
-            if (dataSnapshot.exists()) {
-                binding.status.text = dataSnapshot.child("userID").value.toString()
-                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-                binding.productCondition.text = dataSnapshot.child("userName").value.toString()
-                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-                binding.descriptionHeading.text = dataSnapshot.child("description").value.toString()
-                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-                var category = dataSnapshot.child("priority").value.toString()
-                binding.productCategory.text = category
-                val date = "Selling Date: " + dataSnapshot.child("sellingDate").value.toString()
-
-                var useTime = dataSnapshot.child("usedForTime").value.toString()
-                if (useTime == "") {
-                    useTime = "-"
-                }
-                binding.productUsedFor.text = useTime
-                val uid = dataSnapshot.child("userUID").value
-                fillUser(uid)
-
-
-            }
-        }.addOnFailureListener {
-            TODO("Not yet implemented")
-        }
-
-    }
-
-    private fun fillUser(uid: Any?) {
-        val databaseUser =
-            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
-                .getReference("Users")
-        databaseUser.child(uid.toString()).get().addOnSuccessListener { snapshot ->
-
-            if (snapshot.exists()) {
-                val email = snapshot.child("email").value.toString()
-                val phone = snapshot.child("phonenum").value.toString()
-                val name = snapshot.child("name").value.toString()
-                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-//                binding.sellerName.text = name
-//                binding.sellerEmail.text = email
-//                binding.sellerPhone.text = phone
-//                phoneSeller = phone
-//                emailSeller = email
-            }
-        }.addOnFailureListener {
-            TODO("Not yet implemented")
-        }
-
-    }
+//    private fun fetchDataFromDataBase(items: String) {
+//        val databaseItem =
+//            FirebaseDatabase.getInstance()
+//                .getReference("tickets")
+//
+//        databaseItem.child(items).get().addOnSuccessListener { dataSnapshot ->
+//            //Toast.makeText(applicationContext,dataSnapshot.child("userID").value.toString(),Toast.LENGTH_SHORT).show()
+//            println(dataSnapshot)
+//            if (dataSnapshot.exists()) {
+//                binding.status.text = dataSnapshot.child("userID").value.toString()
+//                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+//                binding.productCondition.text = dataSnapshot.child("userName").value.toString()
+//                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+//                binding.descriptionHeading.text = dataSnapshot.child("description").value.toString()
+//                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+//                var category = dataSnapshot.child("priority").value.toString()
+//                binding.productCategory.text = category
+//                val date = "Selling Date: " + dataSnapshot.child("sellingDate").value.toString()
+//
+//                var useTime = dataSnapshot.child("usedForTime").value.toString()
+//                if (useTime == "") {
+//                    useTime = "-"
+//                }
+//                binding.productUsedFor.text = useTime
+//                val uid = dataSnapshot.child("userUID").value
+//                fillUser(uid)
+//
+//
+//            }
+//        }.addOnFailureListener {
+//            TODO("Not yet implemented")
+//        }
+//
+//    }
+//
+//    private fun fillUser(uid: Any?) {
+//        val databaseUser =
+//            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+//                .getReference("Users")
+//        databaseUser.child(uid.toString()).get().addOnSuccessListener { snapshot ->
+//
+//            if (snapshot.exists()) {
+//                val email = snapshot.child("email").value.toString()
+//                val phone = snapshot.child("phonenum").value.toString()
+//                val name = snapshot.child("name").value.toString()
+//                    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+////                binding.sellerName.text = name
+////                binding.sellerEmail.text = email
+////                binding.sellerPhone.text = phone
+////                phoneSeller = phone
+////                emailSeller = email
+//            }
+//        }.addOnFailureListener {
+//            TODO("Not yet implemented")
+//        }
+//
+//    }
 }
